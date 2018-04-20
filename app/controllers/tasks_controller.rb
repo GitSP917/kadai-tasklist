ec08f35ea36dbcaf44e99956e8f12ab9d85f4d23
GitSP917 <GisSP9675@gmail.com>
2018-04-20 08:49:11 +0000

    課題の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   #before_action :require_user_logged_in, only: [:index, :show]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = current_user.tasks.page(params[:page]).per(3) if current_user
