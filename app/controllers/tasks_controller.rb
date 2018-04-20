1d4473a6015422ec55a81a0ea6a2b36f35799640
GitSP917 <GisSP9675@gmail.com>
2018-04-20 09:58:43 +0000

    課題の修正_2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   #before_action :require_user_logged_in, only: [:index, :show]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.page(params[:page]).per(3) if current_user
@@ -53,9 +53,10 @@ class TasksController < ApplicationController
     
     @task = Task.find(params[:id])
     @task.destroy
-
+    
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
     
   end
   
@@ -70,5 +71,14 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  ######################################################
+  def correct_user
+    @Task = current_user.tasks.find_by(id: params[:id])
+    unless @Task
+      redirect_to root_url
+    end
+  end
+  ######################################################
   
 end
